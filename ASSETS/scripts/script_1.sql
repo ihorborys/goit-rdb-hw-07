08cbb0aefbccac996f52be6401187a90a40e82ec
ihorborys <ihorborys@gmail.com>
2025-07-09 11:09:00 +0300

    Edited task 1, added STR_TO_DATE, LPAD

--- ASSETS/scripts/script_1.sql
+++ ASSETS/scripts/script_1.sql
@@ -1,8 +1,8 @@
 SELECT 
     id,
     date,
-    YEAR(date) AS year,
-    MONTH(date) AS month,
-    DAY(date) AS day
+    YEAR(STR_TO_DATE(date, '%Y-%m-%d')) AS year,
+    LPAD(MONTH(STR_TO_DATE(date, '%Y-%m-%d')), 2, '0') AS month,
+    LPAD(DAY(STR_TO_DATE(date, '%Y-%m-%d')), 2, '0') AS day
 FROM
     hw_theme_3_db.orders;
